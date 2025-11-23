30fe0231f5553f3526e8ab68315aaeafa6369026
Piernik <piernik@piernik.rocks>
2025-11-23 22:24:26 +0100

    web landing page

---
+++ web/app/demo/page.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/components/ui/button";
+import { IconPlus } from "@tabler/icons-react";
+import { useState } from "react";
+
+export default function Page() {
+  const [elements, setElements] = useState([]);
+
+  return (
+    <div className="w-full h-full flex flex-col justify-center items-center gap-8">
+      <h1 className="text-3xl font-bold">Generate Image</h1>
+      <div className="bg-secondary p-6 md:min-w-[500] rounded-2xl min-h-[200] flex justify-center relative">
+        <div className="absolute bottom-5 right-5">
+          <Button size={"icon-lg"} variant={"outline"}>
+            <IconPlus />
+          </Button>
+        </div>
+        <p className="text-lg font-semibold">Elements:</p>
+        <div>
+          {elements.map((e, i) => (
+            <div>test</div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
