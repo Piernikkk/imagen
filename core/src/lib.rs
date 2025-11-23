914c9c797466da539f5eebc352922eccc918d8a4
Piernik <piernik@piernik.rocks>
2025-11-23 15:46:58 +0100

    basic image gen hopefully works, started working on demo

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use color_eyre::eyre::Result;
+
+use img::{
+    codecs::{Codecs, save_png},
+    rgba::Rgba,
+    rgba_image::RgbaImage,
+};
+
+pub struct Canvas {
+    pub image: RgbaImage,
+}
+
+impl Canvas {
+    pub fn new(width: u32, height: u32) -> Self {
+        Self {
+            image: RgbaImage::new(width, height),
+        }
+    }
+
+    pub fn draw_pixel(&mut self, x: u32, y: u32, color: Rgba) -> Result<()> {
+        self.image.set_pixel(x, y, color)
+    }
+
+    pub fn save(&self, path: &str, codec: Codecs) -> Result<()> {
+        match codec {
+            Codecs::PNG => save_png(path, &self.image),
+        }
+    }
 }
--- img/src/codecs/mod.rs
+++ img/src/codecs/mod.rs
@@ -1,7 +1,19 @@
 pub mod png;
 
-use color_eyre::eyre::Result;
+use std::fs::write;
 
-pub fn save_png(path: &str, data: &[u8]) -> Result<()> {
-    todo!();
+use color_eyre::eyre::{Result, eyre};
+
+use crate::{codecs::png::encode_to_png, rgba_image::RgbaImage};
+
+pub enum Codecs {
+    PNG,
+}
+
+pub fn save_png(path: &str, image: &RgbaImage) -> Result<()> {
+    let png_data = encode_to_png(image)?;
+
+    write(path, png_data).map_err(|e| eyre!("Failed to write PNG file to {}: {}", path, e))?;
+
+    Ok(())
 }
